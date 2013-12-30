ef332ff6527f6c42ba2e685a668e7e2b641373b2
Tadeusz Kozak <ted@addlive.com>
2013-12-30 19:43:36 +0100

    Minor - removed an explicit URL

--- AddLive OSX Tutorials/Tutorial9/ALAppDelegate.m
+++ AddLive OSX Tutorials/Tutorial9/ALAppDelegate.m
@@ -69,7 +69,6 @@
     ALConnectionDescriptor* descr = [[ALConnectionDescriptor alloc] init];
 
     descr.scopeId = Consts.SCOPE_ID;
-    descr.url = [NSString stringWithFormat:@"dev01.addlive.com:8004/%@", Consts.SCOPE_ID];
     descr.autopublishAudio = YES;
     descr.autopublishVideo = NO;
     descr.authDetails.userId = rand() % 10000;
